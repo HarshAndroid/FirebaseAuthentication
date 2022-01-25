68d9c66155d9236517a30a8a7d2c60c802c86b51
AndroidHarsh <rajpurohitharsh2020@gmail.com>
2022-01-25 15:22:02 +0530

    2.Create a New Project & Much More...

---
+++ app/src/main/java/com/harshrajpurohit/firebaseauthentication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.harshrajpurohit.firebaseauthentication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.harshrajpurohit.firebaseauthentication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
